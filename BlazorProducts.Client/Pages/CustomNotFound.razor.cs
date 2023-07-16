b56c00f91288f21a40d0b10f25d6036045fe49c2
Master Yi <nguyendinhhao.it@gmail.com>
2023-07-16 15:13:39 +0700

    manage routes

---
+++ BlazorProducts.Client/Pages/CustomNotFound.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class CustomNotFound
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+        public void NavigateToHome()
+        {
+            Navigation.NavigateTo("/");
+        }
+    }
+}
